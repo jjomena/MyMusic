1eb01df74ff31572b1af4565082e46c6b941faff
jjomena <joaquinmena.84@gmail.com>
2016-06-04 17:48:00 -0600

    Reset the Project

---
+++ MyMusic-Project/MyMusic-Project/MyMusic-Project/Models/OurDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyMusic_Project.Models
+{
+    public class OurDbContext:DbContext
+    {
+        public DbSet<UserAccount> userAccount { get; set; }
+    }
+}
