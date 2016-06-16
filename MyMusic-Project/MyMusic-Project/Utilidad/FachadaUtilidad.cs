8a0508fd8e58c52603362539859752d5f52ec284
giobando <gilberth2005@hotmail.com>
2016-06-16 03:07:49 -0600

    Agregue cclase seguridad + su Fachada

---
+++ MyMusic-Project/MyMusic-Project/Utilidad/FachadaUtilidad.cs
@@ -0,0 +1,36 @@
+﻿using MyMusic_Project.ObjetosDeTransferencia.Entidades;
+using MyMusic_Project.Utilidad.Seguridad.AlgoritmosEncritacion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMusic_Project.Utilidad
+{
+    public class FachadaUtilidad
+    {
+        public Login EncriptarContrasena(Login oLogin)
+        {
+            Encriptacion _Encriptar = new Encriptacion();
+            oLogin.StrPassword = _Encriptar.GetSha256(oLogin.StrPassword, oLogin.StrNombreUsuario); //= (new Encriptacion()).GetSha256(oLogin.StrContrasena, oLogin.StrNickname);
+            return oLogin;
+        }
+
+        public string GetSha256(string pStrValor, string pStrSemilla)
+        {
+            Encriptacion _Encriptar = new Encriptacion();
+            String encriptado = _Encriptar.GetSha256(pStrValor, pStrSemilla);
+            return encriptado;// (new Encriptacion()).GetSha256(pStrValor, pStrSemilla);
+        }
+        //public int ObtenerNivelDeAcceso(string sessionId)
+        //{
+        //    return (new NivelesDeAccesoUsuarios()).ObtenerNivelDeAcceso(sessionId);
+        //}
+
+        public Usuario EncriptarContrasena(Usuario oUsuario)
+        {
+            oUsuario.StrContrasena = (new Encriptacion()).GetSha256(oUsuario.StrContrasena, oUsuario.StrLogin);
+            return oUsuario;
+        }
+    }
+}
---
+++ MyMusic-Project/MyMusic-Project/Utilidad/Seguridad/AlgoritmosEncritacion/Encriptacion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace MyMusic_Project.Utilidad.Seguridad.AlgoritmosEncritacion
+{
+    public class Encriptacion
+    {
+        public string GetSha256(string pStrValor, string pStrSemilla)
+        {
+
+
+            UnicodeEncoding ue = new UnicodeEncoding();
+
+            byte[] hashValue;
+            byte[] message = ue.GetBytes(pStrValor + pStrSemilla.Substring(2));//concatena el password+ una parte de la semilla (gilbert, seria = lbert)
+
+            SHA256Managed hashString = new SHA256Managed();
+
+            string hashResult = "";
+
+            hashValue = hashString.ComputeHash(message);
+
+            foreach (byte byteX in hashValue)
+            {
+
+                hashResult += String.Format("{0:x2}", byteX);
+
+            }
+
+            return hashResult;
+
+        }
+
+    }
+}
